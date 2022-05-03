044d1e5cdfc288e37c2bd40ebf4bd19e59cd9ae4
Seven203190709 <103395223+Seven203190709@users.noreply.github.com>
2022-05-03 21:30:37 +0800

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,18 @@
+function myFunction() {
+  var x;
+
+  x = document.getElementById("myText").value;
+
+  if (x=="students") {
+  window.location.replace("")
+  } else if (x=="parents"){
+  window. location.replace("")
+  
+  }
+  else if (x!="students"){
+  alert("Please enter 'students' or 'parents'!")
+  }
+  else if (x!="parents"){
+  alert("Please enter 'students' or 'parents'!")
+  }
+  }
